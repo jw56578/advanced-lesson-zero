1783be2ab3abbc7a4f5cc93494e7b0f3fac4362f
cc123 <cc123@cc123s-MacBook-Pro.local>
2017-03-22 13:18:27 -0500

    testing branch

---
+++ my-first-app/src/index.js
@@ -0,0 +1,29 @@
+//how is import helping us
+//what is it replacing from our normal webpage in plnkr
+//does this look somewhat familiar from node?
+//where is 'react' coming from
+//what javascript thing is the word "React"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//is this still here when i change to master????
+
+
+//what is this replacing from our plnkr example code
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+//we no longer have to call React.createClass
+//we are now just making a class
+//but React.createClass did do important stuff for us so how do we get that
+//this class does need to have some built in functionality from react
+//so we need to "extends" the react class Component
+
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+);
